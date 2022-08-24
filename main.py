1a66234dbace92a735085f65ae76105950d4f6a2
kdm1jkm <kdm1jkmm@gmail.com>
2022-08-24 22:24:58 +0900

    요구사항1 완성

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ MONTH_RANGE = range(3, 8 + 1)
 TEAM = "MBC"
 OUTPUT_NAME = lambda: f"output/{datetime.now().strftime('%Y%m%d-%H%M%S')}.xlsx"
 
-TEAM = input("Enter team: ")
+# TEAM = input("Enter team: ")
 
 
 def request2(data: pd.DataFrame, team=TEAM, month_range=MONTH_RANGE):
@@ -58,51 +58,68 @@ def request2(data: pd.DataFrame, team=TEAM, month_range=MONTH_RANGE):
 
         return list(map(lambda item: (item[0], *item[1]), count.items()))
 
-    return filter_req(data, "홈팀"), filter_req(data, "방문팀")
+    h_data = filter_req(data, "홈팀")
+    a_data = filter_req(data, "방문팀")
+
+    data_1 = defaultdict(lambda: 0)
+    data_2 = defaultdict(lambda: 0)
+
+    for h in h_data:
+        data_1[h[0]] += h[2]
+        data_2[h[0]] += h[1]
+    for a in a_data:
+        data_1[a[0]] += a[1]
+        data_2[a[0]] += a[2]
+
+    def convert(d):
+        return list(map(lambda item: (item[0], item[1]), d.items()))
+
+    return convert(data_1), convert(data_2)
 
 
-def request1(data: pd.DataFrame, month_range=MONTH_RANGE, team=TEAM) -> dict:
+def request1(data: pd.DataFrame, month_range=MONTH_RANGE) -> dict:
     data = data.loc[data["month"].isin(month_range)]
 
-    def filter_req(
-        data: pd.DataFrame, column: str, result_criteria: str
-    ) -> dict[str, int]:
-        data = data.loc[data[column] == team]
-        data = data.loc[data["승"] == result_criteria]
+    홈 = data.loc[data["승"] == "H"]
+    원정 = data.loc[data["승"] == "A"]
 
-        count = defaultdict(lambda: 0)
-        for d in data:
-            count[d] += 1
+    홈_승리 = 홈.rename(columns={"홈팀": "팀"})
+    원정_승리 = 원정.rename(columns={"방문팀": "팀"})
 
-        return count
+    홈_패배 = 원정.rename(columns={"홈팀": "팀"})
+    원정_패배 = 홈.rename(columns={"방문팀": "팀"})
 
-    results: list[dict[str, int]] = []
-    results.append(filter_req(data, "홈팀", "H"))
-    results.append(filter_req(data, "홈팀", "A"))
-    results.append(filter_req(data, "방문팀", "A"))
-    results.append(filter_req(data, "방문팀", "H"))
+    def restructure_frame(data):
+        return data[["팀", "year"]]
 
-    for k, v in results[2].items():
-        results[0][k] += v
-    for k, v in results[3].items():
-        results[1][k] += v
-
-    return list(zip(results[0].keys(), results[0].values())), list(
-        zip(results[1].keys(), results[1].values())
+    count: dict[str, dict[int, list[int, int]]] = defaultdict(
+        lambda: defaultdict(lambda: [0, 0])
     )
+
+    for 승리 in [홈_승리, 원정_승리]:
+        for _, row in 승리.iterrows():
+            count[row["팀"]][row["year"]][0] += 1
+
+    for 패배 in [홈_패배, 원정_패배]:
+        for _, row in 패배.iterrows():
+            count[row["팀"]][row["year"]][1] += 1
+
+    result = dict()
+    for k, v in count.items():
+        result[k] = [(nk, nv[0], nv[1]) for nk, nv in v.items()]
+
+    return result
 
 
 def main():
     # 파일 읽어옴
     with open(FILE_NAME, "rb") as f:
         data = pd.DataFrame(pickle.load(f))
-        print(data)
 
     # 원하는 결과 처라
     results = [None for _ in range(4)]
     results[0], results[1] = request1(data)
     results[2], results[3] = request2(data)
-    print(*results, sep="\n")
 
     # 파일로 저장
     wb = openpyxl.Workbook()
@@ -113,19 +130,19 @@ def main():
 
     ws = wb.active
     ws.title = "행 개수"
-    ws.append(["팀", "행 개수"])
+    ws.append(["팀", "행 개수1"])
     write_excel(ws, results[0])
 
     ws = wb.create_sheet("행 개수2")
     ws.append(["팀", "행 개수"])
     write_excel(ws, results[1])
 
-    ws = wb.create_sheet("홈팀 합계")
-    ws.append(["년도", "홀수번째 데이터 합", "짝수번째 데이터 합"])
+    ws = wb.create_sheet("합계1")
+    ws.append(["년도", "합"])
     write_excel(ws, results[2])
 
-    ws = wb.create_sheet("방문팀 합계")
-    ws.append(["년도", "홀수번째 데이터 합", "짝수번째 데이터 합"])
+    ws = wb.create_sheet("합계2")
+    ws.append(["년도", "합"])
     write_excel(ws, results[3])
 
     if not os.path.isdir("./output"):
@@ -137,8 +154,8 @@ def main():
     dfs = list(map(pd.DataFrame, results))
     dfs[0].columns = ["팀", "행 개수"]
     dfs[1].columns = ["팀", "행 개수"]
-    dfs[2].columns = ["년도", "홀수번째 데이터 합", "짝수번째 데이터 합"]
-    dfs[3].columns = ["년도", "홀수번째 데이터 합", "짝수번째 데이터 합"]
+    dfs[2].columns = ["년도", "합"]
+    dfs[3].columns = ["년도", "합"]
     print(*dfs, sep="\n=========\n")
 
 
